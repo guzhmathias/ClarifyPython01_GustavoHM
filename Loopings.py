3fec3978e870b6ebf9b81a0e20f6baa0dd076cb8
guzhmathias <115371765+guzhmathias@users.noreply.github.com>
2025-08-18 17:14:13 -0300

    Atualização do exercício de looping
    Finalizando o exercício de loopings, utilizando 'for' e 'while'

--- Loopings.py
+++ Loopings.py
@@ -1,1 +1,31 @@
 # Entendendo for e while, conceitos de loopings
+
+palavra = "Tubarão"
+contador = 0
+
+for letra in palavra:
+    print(str(contador)+'-'+letra)
+    contador += 1
+
+cidades = ['São Paulo', 'Osasco', 'POA', 'São Bernardo do Campo']
+
+for cidade in cidades:
+    print(cidade)
+
+linha = '-' * 20
+print(linha)
+print(cidades[3])
+print(linha)
+
+linha1 = '-' * 5
+print (linha1,'Trabalhando com While', linha1)
+botao_executar = True #Boolean true/false
+contador = 0
+
+while botao_executar:
+    print(contador)
+    contador += 1
+    if contador >= 10:
+        botao_executar = False
+
+print('Fim do Looping')
